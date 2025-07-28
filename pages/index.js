ce9aaf4ce13677fdb66c4333bf3fc67e5e145d82
Shaun Lim <slim12252@gmail.com>
2025-07-28 19:54:08 +0800

    feat: increased size of team pics

--- pages/index.js
+++ pages/index.js
@@ -68,24 +68,24 @@ const AnimatedTooltip = ({ items }) => {
                   rotate: rotate,
                   whiteSpace: 'nowrap',
                 }}
-                className="absolute -top-16 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-4 py-2 text-xs shadow-xl"
+                className="absolute -top-20 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center justify-center rounded-md bg-black px-6 py-3 text-sm shadow-xl"
               >
                 <div className="absolute inset-x-10 -bottom-px z-30 h-px w-[20%] bg-gradient-to-r from-transparent via-emerald-500 to-transparent" />
                 <div className="absolute -bottom-px left-10 z-30 h-px w-[40%] bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
-                <div className="relative z-30 text-base font-bold text-white">
+                <div className="relative z-30 text-lg font-bold text-white">
                   {item.name}
                 </div>
-                <div className="text-xs text-white">{item.designation}</div>
+                <div className="text-sm text-white">{item.designation}</div>
               </motion.div>
             )}
           </AnimatePresence>
           <img
             onMouseMove={handleMouseMove}
-            height={100}
-            width={100}
+            height={120}
+            width={120}
             src={item.image}
             alt={item.name}
-            className="relative !m-0 h-14 w-14 rounded-full border-2 border-white object-cover object-top !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105"
+            className="relative !m-0 h-20 w-20 rounded-full border-2 border-white object-cover object-top !p-0 transition duration-500 group-hover:z-30 group-hover:scale-105"
           />
         </div>
       ))}
@@ -98,23 +98,22 @@ const people = [
   {
     id: 1,
     name: 'Shaun Lim',
-    designation: '3x Hackathon Champion',
-    image:
-      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80',
+    designation: 'Chief Monkey Officer',
+    image: '/team/shaun.jpeg',
   },
   {
     id: 2,
     name: 'Lau Jia Sheng',
     designation: 'Evil Overlord',
     image:
-      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+      'https://media.newyorker.com/photos/59095bb86552fa0be682d9d0/master/w_2560%2Cc_limit/Monkey-Selfie.jpg',
   },
   {
     id: 3,
     name: 'Yew Foong Yik',
-    designation: 'ZhinHueys',
+    designation: "ZhinHuey's",
     image:
-      'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+      'https://media.newyorker.com/photos/59095bb86552fa0be682d9d0/master/w_2560%2Cc_limit/Monkey-Selfie.jpg',
   },
   {
     id: 4,
@@ -244,7 +243,7 @@ export default function Home() {
 
             {/* Animated Tooltip Section */}
             <div className="my-12">
-              <h3 className="text-2xl font-semibold mb-6">Our Team</h3>
+              <h3 className="text-2xl font-semibold mb-6">Meet The Team</h3>
               <div className="flex flex-row items-center justify-center mb-10 w-full">
                 <AnimatedTooltip items={people} />
               </div>
